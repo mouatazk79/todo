70f01ffff7cba4aadfdf912b8611e8765ab816af
klaa <mouatazk79@gmail.com>
2022-12-27 19:37:12 +0100

    fixing our repositories with the right methods

--- src/main/java/klaa/mouataz/simpletodo/repositories/PersonRepository.java
+++ src/main/java/klaa/mouataz/simpletodo/repositories/PersonRepository.java
@@ -4,6 +4,11 @@ import klaa.mouataz.simpletodo.model.Person;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepository extends CrudRepository<Person,Long> {
+    Person findPersonById(Long id);
+    List<Person> findAll();
+    void  deletePersonById(Long id);
 }
--- src/main/java/klaa/mouataz/simpletodo/repositories/TodoRepository.java
+++ src/main/java/klaa/mouataz/simpletodo/repositories/TodoRepository.java
@@ -4,6 +4,11 @@ import klaa.mouataz.simpletodo.model.Todo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoRepository extends CrudRepository<Todo,Long> {
+    Todo findTodoById(Long id);
+    List<Todo> findAll();
+    void  deleteTodoById(Long id);
 }
