5f43819f5beb16d72f2acd054322ef236c55cb24
klaa <mouatazk79@gmail.com>
2022-12-27 19:40:04 +0100

    adding the controllers

--- src/main/java/klaa/mouataz/simpletodo/services/impl/TodoServiceImpl.java
+++ src/main/java/klaa/mouataz/simpletodo/services/impl/TodoServiceImpl.java
@@ -3,9 +3,10 @@ package klaa.mouataz.simpletodo.services.impl;
 import klaa.mouataz.simpletodo.model.Todo;
 import klaa.mouataz.simpletodo.repositories.TodoRepository;
 import klaa.mouataz.simpletodo.services.TodoService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class TodoServiceImpl implements TodoService {
     private final TodoRepository todoRepository;
 
