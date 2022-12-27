998db73bf24f7ca857c77b8b3ec99e02a998cc2d
klaa <mouatazk79@gmail.com>
2022-12-27 18:59:43 +0100

    bootstrapping

---
+++ src/test/java/klaa/mouataz/simpletodo/SimpletodoApplicationTests.java
@@ -0,0 +1,13 @@
+package klaa.mouataz.simpletodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpletodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
