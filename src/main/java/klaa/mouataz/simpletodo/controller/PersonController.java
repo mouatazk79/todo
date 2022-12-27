8def38c89656acb954ff02e6bdce253dfd221fac
klaa <mouatazk79@gmail.com>
2022-12-27 19:52:13 +0100

    adding our endpoints to controllers

--- src/main/java/klaa/mouataz/simpletodo/controller/PersonController.java
+++ src/main/java/klaa/mouataz/simpletodo/controller/PersonController.java
@@ -1,9 +1,34 @@
 package klaa.mouataz.simpletodo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import klaa.mouataz.simpletodo.model.Person;
+import klaa.mouataz.simpletodo.services.PersonService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Person")
 public class PersonController {
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping("/{id}")
+    public Person getPerson(@PathVariable Long id){
+     return personService.getPerson(id);
+    }
+    @GetMapping("/all")
+    public List<Person> getPersons(){
+        return personService.getPersons();
+    }
+    @PostMapping("/save")
+    public void savePerson(@RequestBody Person person){
+        personService.savePerson(person);
+    }
+    @DeleteMapping("/{id}")
+    public void deletePerson(@PathVariable Long id){
+        personService.deletePerson(id);
+    }
 }
--- src/main/java/klaa/mouataz/simpletodo/controller/TodoController.java
+++ src/main/java/klaa/mouataz/simpletodo/controller/TodoController.java
@@ -1,9 +1,34 @@
 package klaa.mouataz.simpletodo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import klaa.mouataz.simpletodo.model.Person;
+import klaa.mouataz.simpletodo.model.Todo;
+import klaa.mouataz.simpletodo.services.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Todo")
 public class TodoController {
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+    @GetMapping("/{id}")
+    public Todo getTodo(@PathVariable Long id){
+        return todoService.getTodo(id);
+    }
+    @GetMapping("/all")
+    public List<Todo> getTodos(){
+        return todoService.getTodos();
+    }
+    @PostMapping("/save")
+    public void saveTodo(@RequestBody Todo todo){
+        todoService.saveTodo(todo);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id){
+        todoService.deleteTodo(id);
+    }
 }
